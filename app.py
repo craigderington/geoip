bc8462b587449ecfb02d58f92a3e53090858a359
Craig Derington <craig@craigderington.me>
2016-10-24 13:07:05 -0400

    Initial commit from project scaffold.

---
+++ app.py
@@ -0,0 +1,66 @@
+#! /usr/bin/python
+
+from flask import Flask, request, render_template, redirect, url_for, session, flash
+from flask_googlemaps import GoogleMaps, Map
+import requests
+
+app = Flask(__name__)
+
+#init the extension
+GoogleMaps(app)
+
+# headers
+hdr = {
+    'user-agent': 'Mozilla 5.0/Linux'
+}
+
+# base url
+base_url = 'http://freegeoip.net/'
+
+
+def get_client_ip():
+    client_ip = request.remote_addr
+    return client_ip
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    location = None
+    if request.method == 'POST':
+        ip = request.form['ip_add']
+        format = request.form['format']
+
+    else:
+        ip = '71.43.49.234' #get_client_ip()
+        format = 'json'
+
+    url = base_url + format + '/' + ip
+
+    try:
+        r = requests.get(url, headers=hdr)
+        if r.status_code == 200:
+            location = r.json()
+    except ConnectionError as e:
+        return str(e)
+
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+
+    return render_template(
+            'index.html',
+            location=location,
+            mymap=mymap
+    )
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=5055,
+        debug=True,
+    )
+
