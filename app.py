2edd07449af1d9c154f4e1930014a5dd4c7098b8
Craig Derington <craig@craigderington.me>
2016-10-26 09:51:37 -0400

    Reconfig app for Heroku env.

--- app.py
+++ app.py
@@ -66,9 +66,10 @@ def index():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(
         host='0.0.0.0',
-        port=config.PORT,
+        port=port,
         debug=config.DEBUG,
     )
 
--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 # app settings
 DEBUG = True
-PORT = 5000
 SECRET_KEY = os.urandom(10)
 GOOGLEMAPS_API_KEY = "AIzaSyBeOoT5BWTcghKTeWyjlaZlZ-SZNTjkp10"
 
