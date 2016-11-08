ac8cda99e7d19d9856fa8a47634020c993dd272d
Craig Derington <craig@craigderington.me>
2016-11-08 12:47:03 -0500

    Add WHOIS to app features.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GeoIP
-Geo-Location by Internet Protocol (IP) address web service 
+Geo-Location by Internet Protocol (IP) address web service.
+
+Convert an IP address to a physical location.
+
+Usage:
+* Enter IP address
+* View IP location details
+* View WHOIS data from ARIN.net
+* View map with zoom controls and street view
+
+
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, request, render_template, redirect, url_for, session, flash
 from flask_googlemaps import GoogleMaps, Map
 import requests
+from ipwhois import IPWhois
 import config
 
 app = Flask(__name__)
@@ -34,7 +35,7 @@ def index():
     location = None
 
     if request.method == 'POST':
-        ip = request.form['ip_add']
+        ip = request.form['ip_add'].strip()
         format = 'json'
     else:
         ip = get_client_ip()
@@ -46,15 +47,22 @@ def index():
         r = requests.get(url, headers=hdr)
         if r.status_code == 200:
             location = r.json()
+            ip_address = ip
             mymap = Map(
                 identifier="view-side",
                 lat=location['latitude'],
                 lng=location['longitude'],
                 markers=[(location['latitude'], location['longitude'])],
-                style=('height:500px;'
-                       'width=500px;'
+                style=(
+                    'height:525px;'
+                    'width=500px;'
                 ),
             )
+
+            # whois lookup
+            obj = IPWhois(ip)
+            results = obj.lookup_rdap(depth=1)
+            entity = results['entities'][0]
 
     except requests.exceptions.ConnectionError as e:
         return str(e)
@@ -62,6 +70,9 @@ def index():
     return render_template(
         'index.html',
         location=location,
+        ip_address=ip_address,
+        results=results,
+        entity=entity,
         mymap=mymap
     )
 
